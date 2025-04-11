584bfb511d5346c364654b70a42394408b918b21
hohohoho1223 <dnjsgh1820@naver.com>
2025-04-12 00:15:14 +0900

    Apply .gitignore rules

--- DB/chroma_connector.py
+++
@@ -1,8 +0,0 @@
-
-from langchain_community.vectorstores import Chroma
-from langchain_community.embeddings import HuggingFaceEmbeddings
-
-embedding = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
-
-def load_chroma():
-    return Chroma(persist_directory="chroma_db", embedding_function=embedding)
