28af1fa06db81e29019870b85afa8e4a7cbaaffa
hohohoho1223 <dnjsgh1820@naver.com>
2025-04-12 21:10:07 +0900

    quest-history 기능 추가

--- routes/quest.py
+++ routes/quest.py
@@ -1,14 +1,18 @@
 from fastapi import APIRouter
-from sqlalchemy.orm import Session
+from models.io import QuestInput
 from chains.quest_chain import run_quest_chain
-from models.io import QuestInput, QuestOutput
-from DB.postgres_connector import engine
+from services.postgres import save_to_postgres, fetch_quests_from_db
 from models.quest_db import QuestRecord
 
 router = APIRouter()
 
-@router.post("/generate-quest", response_model=QuestOutput)
+@router.post("/generate-quest")
 async def generate_quest(input_data: QuestInput):
     result = run_quest_chain(input_data)
-    save_quest_result(user_id=input_data.user_id, result=result.model_dump())
+    save_to_postgres(result)
     return result
+
+@router.get("/quest-history/{user_id}")
+async def get_history(user_id: str):
+    history = fetch_quests_from_db(user_id)
+    return {"quests": history}
