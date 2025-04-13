f8089f136b8ffe15a46269d1bc5dd6858056b79b
hohohoho1223 <dnjsgh1820@naver.com>
2025-04-13 19:25:06 +0900

    update db_model.py

--- models/db_model.py
+++ models/db_model.py
@@ -11,10 +11,6 @@ class QuestLog(Base):
     goal = Column(String)
     main_category = Column(String)
     sub_category = Column(String)
-    quests = Column(JSON)  # 전체 퀘스트 JSON 저장
-
-### DB 초기화 및 테이블 생성(한번만 실행만 하면 됨)
-from DB.postgres_connector import engine
     quests = Column(JSON) # ← 여기가 핵심: daily_quests 전체 저장
 
 from DB.postgres_connector import engine 
